ad44ff81a32f6da596d6b59c0f94386f6f77beed
Henryk Dabrowski <henry.dabrow@gmail.com>
2021-04-24 23:59:12 +0200

    Add headers to post request

--- src/api/Auth/Auth.ts
+++ src/api/Auth/Auth.ts
@@ -3,14 +3,17 @@ import Axios, { AxiosRequestConfig } from 'axios';
 export interface Credentials {
   email: string;
   password: string;
-  passwordConfirmation: string;
 }
 
 export const onLogin = async (data: Credentials) => {
   const requestConfig: AxiosRequestConfig = {
     method: 'post',
-    url: process.env.REACT_APP_API_BASE_URL + '/api/v1/registration',
-    data
+    url: process.env.REACT_APP_API_BASE_URL + '/pets',
+    data: data,
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Access-Control-Allow-Origin': '*'
+    }
   }
 
   try {
