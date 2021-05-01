d8996ca08b7409f3e30c912b469561dc1b06723a
Henryk Dabrowski <henry.dabrow@gmail.com>
2021-05-01 23:52:40 +0200

    Refactor registration page, add Yup validations, add custom animation, add color change in case of failed validation

--- src/pages/Auth/Register.tsx
+++ src/pages/Auth/Register.tsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import { Formik, Form } from 'formik';
+import * as Yup from 'yup'
 import { onRegister } from '../../api/Auth/Register';
 import Button from '../../components/atoms/Button'
 import InputField from '../../components/atoms/InputField'
+import InputFieldError from '../../components/atoms/InputFieldError'
 
 const Register = () => {
   const [error, setError] = useState([]);
+  const [formAnimation, setFormAnimation] = useState(false);
   const [formColor, setFormColor] = useState("bg-yellow-50 border-yellow-200")
   const [inputBorder, setInputBorder] = useState("border-yellow-400")
 
@@ -21,12 +24,26 @@ const Register = () => {
     passwordConfirmation: '',
   }
 
+  const registrationSchema = Yup.object().shape({
+    email:
+      Yup.string().email('Invalid email').required('Required'),
+    password:
+      Yup.string().min(8, 'Too Short!').required('Password required!')
+         .matches(/^(?=.*[A-Z]).{0,40}\S$/, 'Must contain capital letter!')
+         .matches(/^(?=.*[0-9]).{8,40}\S$/, 'Must contain number!')
+         .matches(/^(?=.*[!@#$%^&*()]).{8,40}\S$/, 'Must contain special character!'),
+    passwordConfirmation:
+      Yup.string().test('passwordConfirmation',
+                        'Passwords don\'t match!',
+                        function (value) { return this.parent.password === value }),
+  });
+
   const registerUser = async ( values: RegisterFromValues, resetForm: Function ) => {
     const response = await onRegister(values);
 
     if(response && response.error) {
-      console.log(response.error.join("\n"));
       setError(response.error);
+      setFormAnimation(true);
       setFormColor("bg-red-50 border-red-200");
       setInputBorder("border-red-400");
     } else {
@@ -35,53 +52,46 @@ const Register = () => {
   }
 
   return(
-    <div className={"m-6 max-w-xs overflow-auto\
-                     block justify-center\
-                     border-2 rounded-md  " + formColor}
+    <div className={`m-auto my-10 max-w-xs overflow-auto block justify-center border-2 rounded-md
+                     ${formColor} ${formAnimation && "animate-shake"}`}
+          onAnimationEnd={() => setFormAnimation(false)}
     >
-      <div className="mx-14 my-6">
-        <h1 className="text-2xl mx-5 my-6 font-mono text-blue-400">login page</h1>
+      <div className="mx-10 my-6">
+        <h1 className="text-2xl mx-11 my-6 font-mono text-blue-400">login page</h1>
         <Formik
           initialValues={initialValues}
-          onSubmit={(values: RegisterFromValues, actions) => {
-            registerUser(values, actions.resetForm)
-          }}
+          onSubmit={(values: RegisterFromValues, actions) => { registerUser(values, actions.resetForm) }}
+          validationSchema={registrationSchema}
         >
-          <Form>
-            <InputField
-              name='email'
-              placeholder='email'
-              border={inputBorder}
-            />
-            <InputField
-              name='password'
-              placeholder='password'
-              type='password'
-              border={inputBorder}
-            />
-            <InputField
-              name='passwordConfirmation'
-              placeholder='password confirmation'
-              type='password'
-              border={inputBorder}
-            />
-            <div>
-              {
-                error.length > 0 && error.map((e, index) => {
-                  return(
-                    <p
-                      key={index}
-                      className="m-2 font-mono text-xs"
-                    >{e}
-                    </p>
-                  )
-                })
-              }
-            </div>
-            <div className="my-6">
-              <Button label="register"/>
-            </div>
-          </Form>
+          {({ errors, touched }) => (
+            <Form>
+              <InputField
+                name='email'
+                placeholder='email'
+                border={inputBorder}
+              />
+              {errors.email && touched.email ? <InputFieldError error={errors.email}/> : null}
+              <InputField
+                name='password'
+                placeholder='password'
+                type='password'
+                border={inputBorder}
+              />
+              {errors.password && touched.password ? <InputFieldError error={errors.password}/> : null}
+              <InputField
+                name='passwordConfirmation'
+                placeholder='password confirmation'
+                type='password'
+                border={inputBorder}
+              />
+              {errors.passwordConfirmation && touched.passwordConfirmation ?
+                <InputFieldError error={errors.passwordConfirmation}/> : null}
+              {error.map((err, index) => (<InputFieldError error={err} key={index}/>))}
+              <div className="my-6 mx-7">
+                <Button label="register"/>
+              </div>
+            </Form>
+          )}
         </Formik>
       </div>
     </div>
