72e411fd0d8b4c329eeee62c6305906d0d7cc946
Henryk Dabrowski <henry.dabrow@gmail.com>
2021-05-01 02:55:13 +0200

    Improve RegistrationForm component, display error messages from the form, add hooks responsible for color changes in case of error:

--- src/pages/Auth/Register.tsx
+++ src/pages/Auth/Register.tsx
@@ -1,67 +1,84 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { Formik, Form } from 'formik';
 import { onRegister } from '../../api/Auth/Register';
 import Button from '../../components/atoms/Button'
 import InputField from '../../components/atoms/InputField'
 
 const Register = () => {
-  interface IUser {
+  const [error, setError] = useState([]);
+  const [formColor, setFormColor] = useState("bg-yellow-50 border-yellow-200")
+  const [inputBorder, setInputBorder] = useState("border-yellow-400")
+
+  interface RegisterFromValues {
     email: string;
     password: string;
     passwordConfirmation: string;
-
   }
 
-  const [{ email, password, passwordConfirmation }, setCredentials] = useState<IUser>({
+  const initialValues: RegisterFromValues = {
     email: '',
     password: '',
-    passwordConfirmation: ''
-  })
+    passwordConfirmation: '',
+  }
 
-  const [error, setError] = useState([]);
-
-  const Register = async (event: React.FormEvent) => {
-    event.preventDefault();
-    const response = await onRegister({
-      email,
-      password,
-      passwordConfirmation
-    })
+  const registerUser = async ( values: RegisterFromValues, resetForm: Function ) => {
+    const response = await onRegister(values);
 
     if(response && response.error) {
+      console.log(response.error.join("\n"));
       setError(response.error);
+      setFormColor("bg-red-50 border-red-200");
+      setInputBorder("border-red-400");
+    } else {
+      resetForm({});
     }
   }
 
   return(
-    <div className="
-      m-6 max-w-xs overflow-auto
-      block justify-center
-      border-2 rounded-md border-yellow-200
-      bg-yellow-50"
+    <div className={"m-6 max-w-xs overflow-auto\
+                     block justify-center\
+                     border-2 rounded-md  " + formColor}
     >
       <div className="mx-14 my-6">
         <h1 className="text-2xl mx-5 my-6 font-mono text-blue-400">login page</h1>
         <Formik
-          initialValues={{ email: '', password: '', passwordConfirmation: ''}}
-          onSubmit={onRegister}
+          initialValues={initialValues}
+          onSubmit={(values: RegisterFromValues, actions) => {
+            registerUser(values, actions.resetForm)
+          }}
         >
           <Form>
             <InputField
               name='email'
               placeholder='email'
+              border={inputBorder}
             />
             <InputField
               name='password'
               placeholder='password'
               type='password'
+              border={inputBorder}
             />
             <InputField
-              name='password_confirmation'
+              name='passwordConfirmation'
               placeholder='password confirmation'
               type='password'
+              border={inputBorder}
             />
-            <div className="m-6">
+            <div>
+              {
+                error.length > 0 && error.map((e, index) => {
+                  return(
+                    <p
+                      key={index}
+                      className="m-2 font-mono text-xs"
+                    >{e}
+                    </p>
+                  )
+                })
+              }
+            </div>
+            <div className="my-6">
               <Button label="register"/>
             </div>
           </Form>
