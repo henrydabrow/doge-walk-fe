3ce029918f0614553b9adc1c9b42b4e62728cfeb
Henryk Dabrowski <henry.dabrow@gmail.com>
2021-05-04 22:38:50 +0200

    Add link to homepage to router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css';
 import Navbar from './components/molecules/Navbar';
+import Home from './pages/Home';
 import Login from './pages/Auth/Login';
 import Register from './pages/Auth/Register';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
@@ -10,9 +11,8 @@ function App() {
       <Navbar/>
       <Switch>
         <Route path='/login' component={Login}/>
-      </Switch>
-      <Switch>
         <Route path='/register' component={Register}/>
+        <Route path='/' component={Home}/>
       </Switch>
     </BrowserRouter>
   );
