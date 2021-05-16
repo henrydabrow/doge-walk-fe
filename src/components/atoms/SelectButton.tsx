da3702710e0cd8e00ce500cf37fabd74280b3e72
Henryk Dabrowski <henry.dabrow@gmail.com>
2021-05-16 22:53:57 +0200

    Adjust SelectButton component width

--- src/components/atoms/SelectButton.tsx
+++ src/components/atoms/SelectButton.tsx
@@ -4,7 +4,7 @@ export interface Props {
 
 const SelectButton = ({ label }: Props) => (
   <button
-    className={`w-28 px-6 py-2 focus:outline-none
+    className={`w-32 px-6 py-2 focus:outline-none
                 font-mono text-xs text-gray-500
                 border-2 rounded-md
                 bg-white border-green-400
